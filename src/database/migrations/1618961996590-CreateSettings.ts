3819f70096fa881e86cb08121fdea6582ba14b0b
Lucas Moraes <lucasmoraes2@outlook.com.br>
2021-04-20 21:16:50 -0300

    First commit

---
+++ src/database/migrations/1618961996590-CreateSettings.ts
@@ -0,0 +1,43 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSettings1618961996590 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "settings",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "username",
+                        type: "varchar"
+                    },
+                    {
+                        name: "chat",
+                        type: "boolean",
+                        default: true
+                    },
+                    {
+                        name: "updated_at",
+                        type: "tymestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "created_at",
+                        type: "tymestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("settings");
+    }
+
+}
